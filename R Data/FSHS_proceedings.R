3961371c95b244524a8cab58c719c16e6ba975a8
Lisa Play <47617699+witchtropics@users.noreply.github.com>
2021-03-08 21:39:33 -0500

    update of R data, metadata table, creation of plain text files of individual articles for topic mapping

--- R Data/FSHS_proceedings.R
+++ R Data/FSHS_proceedings.R
@@ -8,3 +8,26 @@ p <- ggplot(data = all_df,
 p + geom_point()
 p + geom_boxplot(aes(group = Year))
 p + geom_violin(aes(group= Year))
+
+
+
+
+df<-data.frame(Descriptions_of_Procedure =c(
+  "Anesthesia for lower leg cast application",
+  "Anesthesia for face",
+  "Anesthesia for upper leg",
+  "Dressing change"))
+
+
+for(i in 1:25){
+  write.table(FSHS_AllProceedings[i,9],paste0("TextFiles/", FSHS_AllProceedings[i,5]  ,  ".txt"),row.names=FALSE,col.names = FALSE)
+}
+head(FSHS_AllProceedings)
+str(FSHS_AllProceedings)
+
+
+
+
+x <- c("fsfsd", "fdsfsbvbvbvvdf", "pdfosdfodsfsdf")
+
+
